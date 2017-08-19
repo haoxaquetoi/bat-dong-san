3f149105435b53a4fa0c3d12ea5facb19d4715f4
Quang Tran <tranquang2807@gmail.com>
2017-08-19 16:34:59 +0700

    commit widget image

--- resources/views/frontend/partial/_defaultJs.blade.php
+++ resources/views/frontend/partial/_defaultJs.blade.php
@@ -1,2 +1,3 @@
 <script src="{{ URL::asset('Frontend/js/ctrl/headerCtrl.js') }}"></script>
 <script src="{{ URL::asset('Frontend/js/ctrl/footerCtrl.js') }}"></script>
+<script src="{{ URL::asset('Frontend/js/ctrl/homeSidebarCtrl.js') }}"></script>
---
+++ resources/views/frontend/widget/imageWidget.blade.php
@@ -0,0 +1,7 @@
+<div class="col-xs-12" ng-class="class">
+    <aside>
+        <div class="margin-bottom-15 content-right-img">
+            <img src="@{{image}}" class="img-responsive" alt="" title="@{{title}}" ng-click="action.imgLink()"/>
+        </div>
+    </aside>
+</div>
