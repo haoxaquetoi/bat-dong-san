92bd12f95fb5fae7e2405e3ccbf8fa5692f7ff59
PhamVanHuong <hxaoxaquetoi@gmail.com>
2017-08-22 23:36:48 +0700

    Đẩy db mẫu

--- app/Libs/Config/SettingCrawler.php
+++ app/Libs/Config/SettingCrawler.php
@@ -8,45 +8,60 @@ class SettingCrawler {
 
     protected $columnConfig = [
         'content' => [
-                'title'=>'Tiêu đề',
-                'content'=>'Mô tả',
-                'begin_date'=>'Ngày đăng',
-                'end_date'=>'Ngày kết thúc đăng',
-                'is_sticky'=>'Tin nổi bật',
-                'is_censored'=>'Tin đảm bảo',
-                'is_sold'=>'Tin đã bán',
-                'thumbnail'=>'Ảnh minh họa'
-  
+            'title' => 'Tiêu đề',
+            'content' => 'Mô tả',
+            'begin_date' => 'Ngày đăng',
+            'end_date' => 'Ngày kết thúc đăng',
+            'is_sticky' => 'Tin nổi bật',
+            'is_censored' => 'Tin đảm bảo',
+            'is_sold' => 'Tin đã bán',
+            'thumbnail' => 'Ảnh minh họa'
         ],
         'base' => [
-            'city_id'=>'Tỉnh/Thành phố',
-            'district_id'=>'Quận/Huyện',
-            'village_id'=>'Phường/Xã',
-            'street_id'=>'Đường/Phố',
-            'address'=>'Địa chỉ',
-            'price'=>'Giá đăng',
-            'myself'=>'Chính chủ?'
+            'city_id' => 'Tỉnh/Thành phố',
+            'district_id' => 'Quận/Huyện',
+            'village_id' => 'Phường/Xã',
+            'street_id' => 'Đường/Phố',
+            'address' => 'Địa chỉ',
+            'price' => 'Giá đăng',
+            'myself' => 'Chính chủ?'
         ],
         'other' => [
-            'facade'=>'Mặt tiền',
-            'entry_width'=>'Chiều rộng nối vào',
-            'balcony_direction'=>'Hướng ban công',
-            'number_of_storeys'=>'Số tầng',
-            'number_of_bedrooms'=>'Số phòng',
-            'furniture'=>'Nội thất',
-            'floor_area'=>'Diện tích nhà'
+            'facade' => 'Mặt tiền',
+            'entry_width' => 'Chiều rộng nối vào',
+            'balcony_direction' => 'Hướng ban công',
+            'number_of_storeys' => 'Số tầng',
+            'number_of_bedrooms' => 'Số phòng',
+            'furniture' => 'Nội thất',
+            'floor_area' => 'Diện tích nhà'
         ],
         'contact' => [
-            'name'=>'Họ tên người liên hệ',
-            'address'=>'Địa chỉ liên hệ',
-            'phone'=>'Số điện thoại bàn',
-            'mobile'=>'Số di động',
-            'email'=>'Email'
-
+            'name' => 'Họ tên người liên hệ',
+            'address' => 'Địa chỉ liên hệ',
+            'phone' => 'Số điện thoại bàn',
+            'mobile' => 'Số di động',
+            'email' => 'Email'
         ],
         'slide' => [
             'type' => 'Loại ảnh? [video|youtube|images]',
             'path' => 'Đường dẫn liên kết'
+        ]
+    ];
+    public $arrWebsiteGetData = [
+        ['name' => 'NGuồn 1',
+            'url' => 'http://batdongsan.com.vn',
+            'status' => true,
+            'code' => 'bds1'
+        ],
+        ['name' => 'NGuồn 2',
+            'url' => 'http://batdongsan.com.vn',
+            'status' => true,
+            'code' => 'bds2'
+        ],
+        ['name' => 'NGuồn 3',
+            'url' => 'http://batdongsan.com.vn',
+            'status' => true,
+            'code' => 'bds3'
         ]
     ];
 
--- resources/views/Backend/crawler/main.blade.php
+++ resources/views/Backend/crawler/main.blade.php
@@ -7,92 +7,83 @@
 <script src="{{url('backend')}}/js/factory/services/crawlerService.js"></script>
 @endpush
 
+<?php
+$arrWebsite = app('SettingCrawler')->arrWebsiteGetData;
+?>
 
-<angular ng-controller="crawlerCtrl" ng-cloak="">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <h1>
-            Danh sách nguồn lấy tin <button class="btn btn-primary" ng-click="actions.singleModalCrawler('#modalSingleCrawler')" ><i class="fa fa-plus"></i>&nbsp;Thêm nguồn</button>
-        </h1>
-    </section>
-    <!-- Main content -->
-    <section class="content">
-        <div class="row box-filter">
-            <div class="col-sm-6 col-sm-offset-6">
-                <div  class="dataTables_filter"><label>
-                        <input ng-enter="actions.getAllCrawler(data.filter)" type="search" ng-model="data.filter.keywork" class="form-control " placeholder="Nhập từ khóa" ></label>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="box">
-                    <div class="box-body">
-                        <div class="dataTables_wrapper form-inline dt-bootstrap">
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <table  class="table table-bordered table-hover dataTable" role="grid" >
-                                        <colgroup>
-                                            <col width='5%' />
-                                            <col width='5%' />
-                                            <col width='*' />
-                                            <col width='20%' />
-                                            <col width='17%' />
-                                            <col width='17%' />
-                                            <col width='15%' />
-                                        </colgroup>
-                                        <thead>
-                                            <tr role="row">
-                                                <th>STT</th>
-                                                <th>#</th>
-                                                <th>Tên website</th>
-                                                <th>Url</th>
-                                                <th>Trạng thái</th>
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        Danh sách nguồn lấy tin        
+    </h1>
+</section>
+<!-- Main content -->
+<section class="content">
 
-                                            </tr>
-                                        </thead>
-                                        <tbody>
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="box">
+                <div class="box-body">
+                    <div class="dataTables_wrapper form-inline dt-bootstrap">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <table  class="table table-bordered table-hover dataTable" role="grid" >
+                                    <colgroup>
+                                        <col width='5%' />
+                                        <col width='5%' />
+                                        <col width='*' />
+                                        <col width='20%' />
+                                        <col width='17%' />
+                                        <col width='17%' />
+                                        <col width='15%' />
+                                    </colgroup>
+                                    <thead>
+                                        <tr role="row">
+                                            <th>STT</th>
+                                            <th>#</th>
+                                            <th>Tên website</th>
+                                            <th>Url</th>
+                                            <th>Trạng thái</th>
 
-                                            <tr role="row" class="odd" ng-repeat="crawlerInfo in data.crawler">
-                                                <td >@{{$index + 1}}</td>
-
-                                                <td class="tbl-actions center">
-                                                    <div class="dropdown">
-                                                        <a href="javascript:void(0);" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-bars"></i></a>
-                                                        <ul class="dropdown-menu">
-                                                            <li><a href="{{URL::asset('/admin/crawler/configCrawler')}}" target="_blank" >Cấu hình chi tiết</a></li>
-                                                            <li><a href="javascript:void(0);" ng-click="actions.singleModalCrawler('#modalSingleCrawler', crawlerInfo)">Chi tiết</a></li>
-                                                            <li ng-show="!crawlerInfo.deleted"><a href="javascript:void(0);" ng-click="actions.trashWebsite(crawlerInfo.id)">Xóa</a></li>
-                                                            <li ng-show="crawlerInfo.deleted"><a href="javascript:void(0);" ng-click="actions.publishWebsite(crawlerInfo)">Khôi phục</a></li>
-                                                        </ul>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <a href="javascript:void(0);" ng-click="actions.singleModalCrawler('#modalSingleCrawler', crawlerInfo)">&nbsp;@{{crawlerInfo.website_name}}</a>
-                                                </td>
-                                                <td>
-                                                    @{{crawlerInfo.website_url}}
-                                                </td>
-                                                <td>@{{(crawlerInfo.deleted =='1') ? 'Đã xóa' :'Hoạt động'}}</td>
-                                            </tr>
-
-                                        </tbody>
-                                    </table>
-                                </div>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @for($i = 0; $i < count($arrWebsite); $i++)
+                                        <tr role="row" class="odd" >
+                                            <td >{{$i + 1}}</td>
+                                            <td class="tbl-actions center">
+                                                <div class="dropdown">
+                                                    <a href="javascript:void(0);" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-bars"></i></a>
+                                                    <ul class="dropdown-menu">
+                                                        <li><a href="{{URL::asset('/admin/crawler/configCrawler/').$arrWebsite[$i]['code']}}" >Cấu hình chi tiết</a></li>
+                                                    </ul>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <a href="javascript:void(0);" ng-click="actions.singleModalCrawler('#modalSingleCrawler', crawlerInfo)">&nbsp;{{$arrWebsite[$i]['name']}}</a>
+                                            </td>
+                                            <td>
+                                                {{$arrWebsite[$i]['url']}}
+                                            </td>
+                                            <td>{{($arrWebsite[$i]['status'] ==true) ? 'Đã xóa' :'Hoạt động'}}</td>
+                                        </tr>
+                                        @endfor;
+                                    </tbody>
+                                </table>
                             </div>
-
                         </div>
+
                     </div>
                 </div>
-                <!-- /.box -->
-
             </div>
-            <!-- /.col -->
+            <!-- /.box -->
+
         </div>
-        <!-- /.row -->
-    </section>
-    <!-- /.content -->
-    @include('backend.crawler.modalSingleCrawler')
-</angular>
+        <!-- /.col -->
+    </div>
+    <!-- /.row -->
+</section>
+<!-- /.content -->
+@include('backend.crawler.modalSingleCrawler')
 @endsection
 
