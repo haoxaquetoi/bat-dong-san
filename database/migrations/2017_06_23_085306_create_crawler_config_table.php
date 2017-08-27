8b5ccb82777fb6e8f2ab6fa704ed7d83b2e668dd
PhamVanHuong <hxaoxaquetoi@gmail.com>
2017-08-27 21:08:02 +0700

    a

--- database/migrations/2017_06_23_085306_create_crawler_config_table.php
+++ database/migrations/2017_06_23_085306_create_crawler_config_table.php
@@ -4,26 +4,20 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCrawlerConfigTable extends Migration
-{
+class CreateCrawlerConfigTable extends Migration {
 
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('crawler_config', function (Blueprint $table)
-        {
+    public function up() {
+        Schema::create('crawler_config', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('crawler_id');
             $table->integer('category_id');
-            $table->string('category_xpath',500);
-            $table->string('detail_post_xpath',500);
-            $table->string('url', 500);
-            $table->string('column_name', 50);
-            $table->string('xpath', 500);
+            $table->integer('category_id');
+            $table->string('website_code', 255);
+            $table->text('value');
         });
     }
 
@@ -32,8 +26,7 @@ class CreateCrawlerConfigTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('crawler_config');
     }
 
