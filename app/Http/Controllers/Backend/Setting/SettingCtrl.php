545c5a7d68694c15a79d15134970c2f7f759dbd9
PhamVanHuong <hxaoxaquetoi@gmail.com>
2017-09-17 23:58:08 +0700

    a

--- app/Http/Controllers/Backend/Setting/SettingCtrl.php
+++ app/Http/Controllers/Backend/Setting/SettingCtrl.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\Backend\Setting;
 
 use App\Http\Controllers\Controller;
@@ -6,12 +7,13 @@ use App\Http\Controllers\Controller;
 class SettingCtrl extends Controller {
 
     function main() {
-       
+
         return view('Backend.setting.main');
     }
-    
+
     function detailSetting($code) {
-       
-        return view('Backend.setting.detail.' . $code);
+
+        return view("Backend.setting.detail.$code");
     }
+
 }
